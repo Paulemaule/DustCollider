8e2ac4433a447373d6401da48f1c1e1d5f09dc9f
Paul Zürn <paul.zuern@stud.uni-heidelberg.de>
2025-01-16 18:41:45 +0000

    Added makros for more conventient error handling.

---
+++ src/errors.cuh
@@ -0,0 +1,16 @@
+#pragma once
+
+// A makro that throws a runtime error with a predefined structure and variable description.
+#define PANIC(description) throw std::runtime_error(std::string("ERROR: ") + description + " | " + "Source: " + __FILE__ + ":" + std::to_string(__LINE__) + "\n")
+
+// A makro that checks for the last cuda error and throws an exception if one is encountered.
+#define CUDA_LAST_ERROR_CHECK()                                              \
+    do {                                                                     \
+        cudaError_t err = cudaGetLastError();                                \
+        if (err != cudaError::cudaSuccess) {                                 \
+            throw std::runtime_error(std::string("CUDA ERROR: ") +           \
+                                     cudaGetErrorString(err) + " | "         \
+                                     "Detection: " + __FILE__ + ":" +        \
+                                     std::to_string(__LINE__));              \
+        }                                                                    \
+    } while (0)
