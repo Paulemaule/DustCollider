361809125a4575244cc085176f3d38aa5bf3ac33
Paul Zürn <paul.zuern@stud.uni-heidelberg.de>
2025-01-30 16:54:51 +0000

    Several Bug fixes. Adjusted typing of several quantities. Fixed issued with memset and removed a function that is not supported by CUDA.

--- src/physics/state.cuh
+++ src/physics/state.cuh
@@ -2,8 +2,8 @@
 
 #include <iostream>
 
-#include "/utils/printing.cuh"
-#include "/utils/errors.cuh"
+#include "../utils/printing.cuh"
+#include "../utils/errors.cuh"
 
 /**
  * @brief A struct of pointers to host memory of arrays containing system state information.
@@ -16,11 +16,11 @@ typedef struct {
     double3* magnetization_change;          // Array - Nmon * sizeof(<T>)
     double3* force;                         // Array - Nmon * sizeof(<T>)
     double3* torque;                        // Array - Nmon * sizeof(<T>)
-    double3* contact_compression;           // Matrix - Nmon * Nmon * sizeof(<T>)
-    double3* contact_twist;                 // Matrix - Nmon * Nmon * sizeof(<T>)
+    double* contact_compression;            // Matrix - Nmon * Nmon * sizeof(<T>)
+    double* contact_twist;                  // Matrix - Nmon * Nmon * sizeof(<T>)
     double3* contact_pointer;               // Matrix - Nmon * Nmon * sizeof(<T>)
     double3* contact_normal;                // Matrix - Nmon * Nmon * sizeof(<T>)
-    double3* contact_rotation;              // Matrix - Nmon * Nmon * sizeof(<T>)
+    double4* contact_rotation;              // Matrix - Nmon * Nmon * sizeof(<T>)
 } hostState;
 
 /**
@@ -34,11 +34,11 @@ typedef struct {
     double3* magnetization_change;          // Array - Nmon * sizeof(<T>)
     double3* force;                         // Array - Nmon * sizeof(<T>)
     double3* torque;                        // Array - Nmon * sizeof(<T>)
-    double3* contact_compression;           // Matrix - Nmon * Nmon * sizeof(<T>)
-    double3* contact_twist;                 // Matrix - Nmon * Nmon * sizeof(<T>)
+    double* contact_compression;            // Matrix - Nmon * Nmon * sizeof(<T>)
+    double* contact_twist;                  // Matrix - Nmon * Nmon * sizeof(<T>)
     double3* contact_pointer;               // Matrix - Nmon * Nmon * sizeof(<T>)
     double3* contact_normal;                // Matrix - Nmon * Nmon * sizeof(<T>)
-    double3* contact_rotation;              // Matrix - Nmon * Nmon * sizeof(<T>)
+    double4* contact_rotation;              // Matrix - Nmon * Nmon * sizeof(<T>)
 } deviceState;
 
 /**
@@ -56,11 +56,11 @@ void state_allocateHostMemory(hostState& state, size_t Nmon) {
     CHECK_CUDA(cudaMallocHost(& state.magnetization_change, Nmon * sizeof(double3)));
     CHECK_CUDA(cudaMallocHost(& state.force, Nmon * sizeof(double3)));
     CHECK_CUDA(cudaMallocHost(& state.torque, Nmon * sizeof(double3)));
-    CHECK_CUDA(cudaMallocHost(& state.contact_compression, Nmon * Nmon * sizeof(double3)));
-    CHECK_CUDA(cudaMallocHost(& state.contact_twist, Nmon * Nmon * sizeof(double3)));
+    CHECK_CUDA(cudaMallocHost(& state.contact_compression, Nmon * Nmon * sizeof(double)));
+    CHECK_CUDA(cudaMallocHost(& state.contact_twist, Nmon * Nmon * sizeof(double)));
     CHECK_CUDA(cudaMallocHost(& state.contact_pointer, Nmon * Nmon * sizeof(double3)));
     CHECK_CUDA(cudaMallocHost(& state.contact_normal, Nmon * Nmon * sizeof(double3)));
-    CHECK_CUDA(cudaMallocHost(& state.contact_rotation, Nmon * Nmon * sizeof(double3)));
+    CHECK_CUDA(cudaMallocHost(& state.contact_rotation, Nmon * Nmon * sizeof(double4)));
 }
 
 /**
@@ -77,11 +77,11 @@ void state_allocateDeviceMemory(deviceState& state, size_t Nmon) {
     CHECK_CUDA(cudaMalloc(& state.magnetization_change, Nmon * sizeof(double3)));
     CHECK_CUDA(cudaMalloc(& state.force, Nmon * sizeof(double3)));
     CHECK_CUDA(cudaMalloc(& state.torque, Nmon * sizeof(double3)));
-    CHECK_CUDA(cudaMalloc(& state.contact_compression, Nmon * Nmon * sizeof(double3)));
-    CHECK_CUDA(cudaMalloc(& state.contact_twist, Nmon * Nmon * sizeof(double3)));
+    CHECK_CUDA(cudaMalloc(& state.contact_compression, Nmon * Nmon * sizeof(double)));
+    CHECK_CUDA(cudaMalloc(& state.contact_twist, Nmon * Nmon * sizeof(double)));
     CHECK_CUDA(cudaMalloc(& state.contact_pointer, Nmon * Nmon * sizeof(double3)))
     CHECK_CUDA(cudaMalloc(& state.contact_normal, Nmon * Nmon * sizeof(double3)));
-    CHECK_CUDA(cudaMalloc(& state.contact_rotation, Nmon * Nmon * sizeof(double3)));
+    CHECK_CUDA(cudaMalloc(& state.contact_rotation, Nmon * Nmon * sizeof(double4)));
 }
 
 /**
@@ -165,11 +165,11 @@ void state_pushToDevice(hostState& host_state, deviceState& device_state, size_t
         cudaMemcpyKind::cudaMemcpyHostToDevice));
     CHECK_CUDA(cudaMemcpy(
         device_state.contact_compression, host_state.contact_compression, 
-        Nmon * Nmon * sizeof(double3), 
+        Nmon * Nmon * sizeof(double), 
         cudaMemcpyKind::cudaMemcpyHostToDevice));
     CHECK_CUDA(cudaMemcpy(
         device_state.contact_twist, host_state.contact_twist,
-        Nmon * Nmon * sizeof(double3),
+        Nmon * Nmon * sizeof(double),
         cudaMemcpyKind::cudaMemcpyHostToDevice));
     CHECK_CUDA(cudaMemcpy(
         device_state.contact_pointer, host_state.contact_pointer,
@@ -181,7 +181,7 @@ void state_pushToDevice(hostState& host_state, deviceState& device_state, size_t
         cudaMemcpyKind::cudaMemcpyHostToDevice));
     CHECK_CUDA(cudaMemcpy(
         device_state.contact_rotation, host_state.contact_rotation,
-        Nmon * Nmon * sizeof(double3),
+        Nmon * Nmon * sizeof(double4),
         cudaMemcpyKind::cudaMemcpyHostToDevice));
 }
 
@@ -224,11 +224,11 @@ void state_pullFromDevice(deviceState& device_state, hostState& host_state, size
         cudaMemcpyKind::cudaMemcpyDeviceToHost));
     CHECK_CUDA(cudaMemcpy(
         host_state.contact_compression, device_state.contact_compression, 
-        Nmon * Nmon * sizeof(double3), 
+        Nmon * Nmon * sizeof(double), 
         cudaMemcpyKind::cudaMemcpyDeviceToHost));
     CHECK_CUDA(cudaMemcpy(
         host_state.contact_twist, device_state.contact_twist,
-        Nmon * Nmon * sizeof(double3),
+        Nmon * Nmon * sizeof(double),
         cudaMemcpyKind::cudaMemcpyDeviceToHost));
     CHECK_CUDA(cudaMemcpy(
         host_state.contact_pointer, device_state.contact_pointer,
@@ -240,50 +240,27 @@ void state_pullFromDevice(deviceState& device_state, hostState& host_state, size
         cudaMemcpyKind::cudaMemcpyDeviceToHost));
     CHECK_CUDA(cudaMemcpy(
         host_state.contact_rotation, device_state.contact_rotation,
-        Nmon * Nmon * sizeof(double3),
+        Nmon * Nmon * sizeof(double4),
         cudaMemcpyKind::cudaMemcpyDeviceToHost));
 }
 
 /**
- * @brief Sets the values of all fields of a host state to a specified value.
+ * @brief Sets the values of all fields of a host state to 0.
  * 
  * @param &state: The device state whos values are to be set.
- * @param value: The value the fields are to be set to.
  * @param Nmon: The number of monomers.
  */
-void state_setValueHost(hostState& state, double value, size_t Nmon) {
-    memset(state.position, value, Nmon * sizeof(double3));
-    memset(state.magnetization, value, Nmon * sizeof(double3));
-    memset(state.velocity, value, Nmon * sizeof(double3));
-    memset(state.omega, value, Nmon * sizeof(double3));
-    memset(state.magnetization_change, value, Nmon * sizeof(double3));
-    memset(state.force, value, Nmon * sizeof(double3));
-    memset(state.torque, value, Nmon * sizeof(double3));
-    memset(state.contact_compression, value, Nmon * Nmon * sizeof(double3));
-    memset(state.contact_twist, value, Nmon * Nmon * sizeof(double3));
-    memset(state.contact_pointer, value, Nmon * Nmon * sizeof(double3));
-    memset(state.contact_normal, value, Nmon * Nmon * sizeof(double3));
-    memset(state.contact_twist, value, Nmon * Nmon * sizeof(double3)); 
-}
-
-/**
- * @brief Sets the values of all fields of a device state to a specified value.
- * 
- * @param &state: The device state whos values are to be set.
- * @param value: The value the fields are to be set to.
- * @param Nmon: The number of monomers.
- */
-void state_setValueDevice(deviceState& state, double value, size_t Nmon) {
-    CHECK_CUDA(cudaMemset(state.position, value, Nmon * sizeof(double3)));
-    CHECK_CUDA(cudaMemset(state.magnetization, value, Nmon * sizeof(double3)));
-    CHECK_CUDA(cudaMemset(state.velocity, value, Nmon * sizeof(double3)));
-    CHECK_CUDA(cudaMemset(state.omega, value, Nmon * sizeof(double3)));
-    CHECK_CUDA(cudaMemset(state.magnetization_change, value, Nmon * sizeof(double3)));
-    CHECK_CUDA(cudaMemset(state.force, value, Nmon * sizeof(double3)));
-    CHECK_CUDA(cudaMemset(state.torque, value, Nmon * sizeof(double3)));
-    CHECK_CUDA(cudaMemset(state.contact_compression, value, Nmon * Nmon * sizeof(double3)));
-    CHECK_CUDA(cudaMemset(state.contact_twist, value, Nmon * Nmon * sizeof(double3)));
-    CHECK_CUDA(cudaMemset(state.contact_pointer, value, Nmon * Nmon * sizeof(double3)));
-    CHECK_CUDA(cudaMemset(state.contact_normal, value, Nmon * Nmon * sizeof(double3)));
-    CHECK_CUDA(cudaMemset(state.contact_twist, value, Nmon * Nmon * sizeof(double3)));
+void state_clearHost(hostState& state, size_t Nmon) {
+    memset(state.position, 0, Nmon * sizeof(double3));
+    memset(state.magnetization, 0, Nmon * sizeof(double3));
+    memset(state.velocity, 0, Nmon * sizeof(double3));
+    memset(state.omega, 0, Nmon * sizeof(double3));
+    memset(state.magnetization_change, 0, Nmon * sizeof(double3));
+    memset(state.force, 0, Nmon * sizeof(double3));
+    memset(state.torque, 0, Nmon * sizeof(double3));
+    memset(state.contact_compression, 0, Nmon * Nmon * sizeof(double));
+    memset(state.contact_twist, 0, Nmon * Nmon * sizeof(double));
+    memset(state.contact_pointer, 0, Nmon * Nmon * sizeof(double3));
+    memset(state.contact_normal, 0, Nmon * Nmon * sizeof(double3));
+    memset(state.contact_twist, 0, Nmon * Nmon * sizeof(double4)); 
 }
